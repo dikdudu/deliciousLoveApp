5fe390d81b4e1e5cb06e3d44a20762a3697172e3
dikdudu <dikdudu@hotmail.com>
2020-04-22 12:35:12 -0300

    adcionei algumas rotas

--- lib/app/modules/base/base_module.dart
+++ lib/app/modules/base/base_module.dart
@@ -1,8 +1,11 @@
 import 'package:deliciousloveapp/app/modules/clients/pages/add_client_page.dart';
 import 'package:deliciousloveapp/app/modules/clients/clients_controller.dart';
+import 'package:deliciousloveapp/app/modules/clients/pages/clients_page.dart';
+import 'package:deliciousloveapp/app/modules/home/home_page.dart';
 import 'package:deliciousloveapp/app/modules/login/login_controller.dart';
 import 'package:deliciousloveapp/app/modules/login/login_page.dart';
 import 'package:deliciousloveapp/app/modules/orders/pages/add_order_page.dart';
+import 'package:deliciousloveapp/app/modules/orders/pages/oders_page.dart';
 import 'package:deliciousloveapp/app/shared/controllers/navigation_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -20,9 +23,11 @@ class BaseModule extends ChildModule{
   List<Router> get routers => [
     Router('/', child: (_, args) => BasePage()),
     Router('/login', child: (_, args) => LoginPage()),
+    Router('/home', child: (_, args) => HomePage()),
+    Router('/orders', child: (_, args) => OrdersPage()),
+    Router('/orders/addorder', child: (_, args) => AddOrderPage()),
+    Router('/clients', child: (_, args) => ClientsPage()),
     Router('/clients/addclient', child: (_, args) => AddClientPage()),
-    Router('/orders/addorder', child: (_, args) => AddOrderPage())
-
   ];
 
 }
