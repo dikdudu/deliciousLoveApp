008e0fb89d4bb7b66b5fc98d9a8d269fab2277b0
dikdudu <dikdudu@hotmail.com>
2020-05-16 14:57:05 -0300

    create module client

--- lib/app/modules/clients/pages/client/client_page.dart
+++ lib/app/modules/clients/pages/client/client_page.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'client_controller.dart';
 
-class ClientPage extends StatelessWidget {
+class ClientPage extends StatefulWidget {
+  final String title;
+  const ClientPage({Key key, this.title = "Client"}) : super(key: key);
+
+  @override
+  _ClientPageState createState() => _ClientPageState();
+}
+
+class _ClientPageState extends ModularState<ClientPage, ClientController> {
+  //use 'controller' variable to access controller
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
   }
 }
