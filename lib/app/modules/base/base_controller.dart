50b8c5fa12774e3acef6d9b92f6e3a65f558c225
dikdudu <dikdudu@hotmail.com>
2020-05-17 11:30:54 -0300

    organ...

---
+++ lib/app/modules/base/base_controller.dart
@@ -0,0 +1,9 @@
+import 'package:mobx/mobx.dart';
+
+part 'base_controller.g.dart';
+
+class BaseController = _BaseControllerBase with _$BaseController;
+
+abstract class _BaseControllerBase with Store {
+
+}
